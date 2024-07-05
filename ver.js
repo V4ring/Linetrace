c9831c08aab1c3a7ffe29fb2c27f6b57a776943f
V4ring <163661698+V4ring@users.noreply.github.com>
2024-07-05 11:24:18 +1000

    Add files via upload

---
+++ ver.js
@@ -0,0 +1,18 @@
+let tg = window.Telegram.WebApp
+
+tg.expand();
+
+tg.MainButton.text = "Changed Text";
+tg.MainButton.setText("Changed Texto");
+tg.MainButton.textColor = "White";
+tg.MainButton.color = "Green";
+tg.MainButton.setParams({"color": "Green"});
+
+btn.addEventListener('click', function(){
+    if (tg.MainButton.isVisible) {
+        tg.MainButton.hide()
+    }
+    else {
+        tg.MainButton.show()
+    }
+});
